6c3fe85e7005a34553c49b698fbb2edaa2be9fe7
Made Manik Datu Yasa <manikyasa@gmail.com>
2022-05-24 11:33:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const movies = [
+  {
+    id: 1,
+    name: "Matrix",
+    country: 9,
+    collection: 300, //in CRs
+    releasedOn: 1999,
+  },
+  {
+    id: 2,
+    name: "Tere Nam",
+    country: 3,
+    collection: 101,
+    releasedOn: 2004,
+  },
+  {
+    id: 3,
+    name: "Bahubali",
+    country: 4,
+    collection: 500,
+    releasedOn: 1987,
+  },
+];
 
 function App() {
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState("albums");
+
+  const sortArray = (type) => {
+    const types = {
+      country: "country",
+      collection: "collection",
+      releasedOn: "releasedOn",
+    };
+
+    const sortProperty = types[type];
+    const sorted = [...movies].sort((a, b) => b[sortProperty] - a[sortProperty]);
+    setData(sorted);
+    console.log(sortProperty);
+  };
+
+  useEffect(() => {
+    sortArray(sortType);
+  }, [sortType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select name="" id="" onChange={(e) => setSortType(e.target.value)}>
+        <option value="country">Country</option>
+        <option value="collection">Collection</option>
+        <option value="releasedOn">Release Date</option>
+      </select>
+
+      {data.map((movie) => (
+        <div key={movie.id} style={{ margin: "30px" }}>
+          <div>{`Movie : ${movie.name}`}</div>
+          <div>{`Country : ${movie.country}`}</div>
+          <div>{`Collection : ${movie.collection}`}</div>
+          <div>{`Year : ${movie.releasedOn}`}</div>
+        </div>
+      ))}
     </div>
   );
 }
